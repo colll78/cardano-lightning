5a48c1dc758e02f830d03ad9febaebdb6f57cbc2
waalge <waagle@jes>
2024-10-29 09:36:29 +0000

    configure pre-commit

--- flake.nix
+++ flake.nix
@@ -4,19 +4,17 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    pre-commit-hooks-nix.url = "github:hercules-ci/pre-commit-hooks.nix/flakeModule";
-    pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    git-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
-    cardano-addresses.url = "github:IntersectMBO/cardano-addresses";
-    cardano-cli.url = "github:IntersectMBO/cardano-cli";
-    bech32.url = "github:IntersectMBO/bech32";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; }
       {
         imports = [
-          inputs.pre-commit-hooks-nix.flakeModule
+          inputs.git-hooks-nix.flakeModule
           inputs.treefmt-nix.flakeModule
         ];
         systems = [ "x86_64-linux" "aarch64-darwin" ];
@@ -34,7 +32,8 @@
               };
             };
           };
-
+          pre-commit.settings.hooks.treefmt.enable = true;
+          # NOTE: You can also use `config.pre-commit.devShell`
           devShells.default =
           pkgs.mkShell {
             nativeBuildInputs = [
@@ -42,16 +41,13 @@
             ]
             ;
             shellHook = ''
+              ${config.pre-commit.installationScript}
               echo 1>&2 "Welcome to the development shell!"
             '';
             name = "cardano-lightning";
             # Let's keep this "path discovery techinque" here for refernece:
             # (builtins.trace (builtins.attrNames inputs.cardano-addresses.packages.${system}) inputs.cardano-cli.packages)
             packages = with pkgs; [
-              inputs.cardano-cli.packages.${system}."cardano-cli:exe:cardano-cli"
-              inputs.cardano-addresses.packages.${system}."cardano-addresses-cli:exe:cardano-address"
-              inputs.bech32.packages.${system}."bech32:exe:bech32"
-              b2sum
             ];
           };
         };
